cabd314a2b1d71053296e4d18096c4b9075ea491
Nikolay Dimitrow <nikidimitrow@users.noreply.github.com>
2020-03-19 08:49:22 +0200

    Solution

--- solution/P1108-defanging_an_ip_address.java
+++ solution/P1108-defanging_an_ip_address.java
@@ -2,6 +2,14 @@
 
 class Solution {
     public String defangIPaddr(String address) {
-        
+        StringBuilder str = new StringBuilder();
+
+        for(int i = 0; i < address.length(); i++) {
+            char ch = address.charAt(i);
+            if (ch == '.') str.append("[.]");
+            else str.append(ch);
+        }
+
+        return str.toString();
     }
 }
